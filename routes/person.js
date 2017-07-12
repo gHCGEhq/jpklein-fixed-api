560aa317c7ba2d929abfb07e3c0d7bb4ff379bd3
jpklein <jpklein@gmail.com>
2017-07-11 21:48:53 -0700

    Passes tests 1 & 2

--- routes/person.js
+++ routes/person.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 function Person(body) {
   this.name = body.name;
-  this.pets = body.pets;
+  this.pets = body.pets || '';
   this.apples = 12;
 }
 
 Person.prototype.greet = function() {
-  this.greeting = this.name = 'Hi ' + this.name + ', how are you?';
+  this.greeting = 'Hi ' + this.name + ', how are you?';
   return this.greeting;
 };
 
-Person.prototype.howManyApples = function(first_argument) {
-  return this.name + ' has ' + apples + ' apples';
+Person.prototype.howManyApples = function() {
+  return this.name + ' has ' + this.apples + ' apples';
 };
 
 Person.prototype.greetPet = function(pet) {
   return 'Hi ' + pet + ', YOU\'RE JUST SO FLUFFY! :O ';
 };
 
-Person.prototype.greetPets = function(first_argument) {
+Person.prototype.greetPets = function() {
   this.petGreeting = [];
   if (typeof this.pets === 'array') {
     pets.forEach(function(pet) {
       this.petGreeting.push(this.greetPet(pet));
     });
   } else {
-    this.petGreeting.push(this.greetPet(pet));
+    this.petGreeting.push(this.greetPet(this.pets));
   }
   return this.petGreeting.join();
 };
@@ -35,7 +35,7 @@ Person.prototype.greetPets = function(first_argument) {
 router.post('/', function(req, res, next) {
   var _person = new Person(req.body);
   res.json({
-  	greeting: _person.greet(),
+    greeting: _person.greet(),
     apples: _person.howManyApples(),
     greetPets: _person.greetPets()
   });
